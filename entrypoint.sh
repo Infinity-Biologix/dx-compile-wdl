9b91a1e8d7466447de63069129096f7e5783df4a
akbog <alex.bogdanowicz@nyu.edu>
2021-07-30 10:01:11 -0400

    feature: added inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ echo "Project id: $1"
 echo "Overwrite: $2"
 echo "Workflow directory: $3"
 echo "Workflow Name: $4"
-echo "Destination Path: $5"
+echo "Input Json Name: $5"
+echo "Destination Path: $6"
 
 echo "Logging into dnanexus"
 dx login --token $DX_TOKEN --noprojects
@@ -22,9 +23,9 @@ echo "Selected dnanexus project: $1"
 echo "Compiling workflow applet"
 
 if [ "$2" = 'true' ]; then
-    java -jar /tmp/dxCompiler-2.4.7.jar compile $WDL_FILE_NAME --inputs $INPUT_JSON_NAME --project $1 --destination $5
+    java -jar /tmp/dxCompiler-2.4.7.jar compile $4 --inputs $5 --project $1 --destination $6
 else
-    java -jar /tmp/dxCompiler-2.4.7.jar compile $WDL_FILE_NAME --inputs $INPUT_JSON_NAME --project $1 --destination $5
+    java -jar /tmp/dxCompiler-2.4.7.jar compile $4 --inputs $5 --project $1 --destination $6
 fi
 
 echo ::set-output name=project-id::$1
