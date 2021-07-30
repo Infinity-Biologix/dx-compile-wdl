0670d1b28a45533fd1c99cc19f421daca8cf2e8c
akbog <alex.bogdanowicz@nyu.edu>
2021-07-30 13:56:46 -0400

    feature: unbound method

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,17 @@
 set -uex
 
 echo "Project id: $1"
-echo "Workflow directory: $3"
-echo "Workflow Name: $4"
-echo "Input Json Name: $5"
-echo "Destination Path: $6"
+echo "Workflow directory: $2"
+echo "Workflow Name: $3"
+echo "Input Json Name: $4"
+echo "Destination Path: $5"
 
 echo "Changing into workspace directory & listing contents"
 cd /github/workspace
 echo listing directory: $(ls)
 echo "Changing into applet directory & listing contents"
-cd $3
+cd $2
 ls -alh
-CMD="java -jar /tmp/dxCompiler-2.4.7.jar compile $4 --inputs $5 --project $1 --destination $6"
+CMD="java -jar /tmp/dxCompiler-2.4.7.jar compile $3 --inputs $4 --project $1 --destination $5"
 workflow_id=$(eval "$CMD")
 echo ::set-output name=workflow_id::"$workflow_id"
