5651e75e04bcc97817d96ba2df6514d4e158762c
Alexander Bogdanowicz <55267935+akbog@users.noreply.github.com>
2021-08-26 15:15:44 -0400

    fix: no need for input-json

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@ set -uex
 echo "Project id: $1"
 echo "Workflow directory: $2"
 echo "Workflow Name: $3"
-echo "Input Json Name: $4"
-echo "Destination Path: $5"
+echo "Destination Path: $4"
 
 echo "Changing into workspace directory & listing contents"
 cd /github/workspace
@@ -13,6 +12,6 @@ echo listing directory: $(ls)
 echo "Changing into applet directory & listing contents"
 cd $2
 ls -alh
-CMD="java -jar /dxCompiler.jar compile $3 --inputs $4 --project $1 --destination $5"
+CMD="java -jar /dxCompiler.jar compile $3 --project $1 --destination $4"
 workflow_id=$(eval "$CMD")
 echo ::set-output name=workflow_id::"$workflow_id"
